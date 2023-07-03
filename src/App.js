fd084b4dcd0c72b8cce9101310f281a5d8ecf01d
Peter McClelland <peterwmcclelland@gmail.com>
2023-07-03 14:53:51 -0500

    login feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
+import SignUpForm from './routes/SignUp/SignUpForm';
+import LoginForm from './routes/LoginForm/LoginForm';
 import AddSpot from "./components/AddSpot";
 import Spots from "./components/Spot/Spots";
 import SpotDetail from "./components/Spot/SpotDetail";
 
 
 function App() {
+ 
   return (
     <BrowserRouter>
       <React.Fragment>
@@ -21,6 +23,8 @@ function App() {
             <Route path="/add" element={<AddSpot />} />
             <Route path="/spots" element={<Spots />} />
             <Route path="/spots/:id" element={<SpotDetail />} />
+            <Route exact path="/login" element={<LoginForm />} />
+            <Route exact path="/signup" element={<SignUpForm />} />
             <Route
               path="/spot-list"
               element={<Navigate to="/Spot-List" />}
